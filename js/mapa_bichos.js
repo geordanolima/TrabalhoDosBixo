bf9b0d5fde82e5badfb556d68acdcaba643d7e3b
Geordano Lima <geordanofl@gmail.com>
2018-07-03 21:15:56 -0300

    correções no js

--- js/mapa_bichos.js
+++ js/mapa_bichos.js
@@ -13,8 +13,23 @@
         }
         
     }
-   var bicho = new Bichos('charizard', 'img/bichosmapa/006.png', '',10,5,6,-30.865500, -51.800791);
-   console.log(bicho.icone);
+
+    function sorteio(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+   var bicharedo=[];
+    var bicho = new Bichos('Charizard', 'img/bichosmapa/001.png', '',78,84,78,-30.865033, -51.801373);
+    bicharedo.push(bicho);
+    var bicho = new Bichos('Gyarados', 'img/bichosmapa/002.png', '',95,125,79,-30.866258, -51.800719);
+    bicharedo.push(bicho);
+    var bicho = new Bichos('Rhyhorn', 'img/bichosmapa/003.png', '',80,85,95,-30.865783, -51.799899);
+    bicharedo.push(bicho);
+   console.log(bicharedo.length);
+   var indicesorteado=sorteio(0,bicharedo.length);
+   
 
       var map;
       function initMap() {
@@ -24,16 +39,23 @@
           mapTypeId: 'satellite'
         });
         
-        var contentString = '<p>'+bicho.nome+'</p>';
-        bicho.conteudo=contentString;
+
+        var contentString = '<p><center>'+bicharedo[indicesorteado].nome+'</center></p>'+
+        '<ul>'+
+            '<li>Vida: '+bicharedo[indicesorteado].vida+'</li>'+
+            '<li>Ataque: '+bicharedo[indicesorteado].ataque+'</li>'+
+            '<li>Defesa: '+bicharedo[indicesorteado].defesa+'</li>'
+        
+        +'</ul>';
+        bicharedo[indicesorteado].conteudo=contentString;
         var infowindow = new google.maps.InfoWindow({
-          content: bicho.conteudo
+          content: bicharedo[indicesorteado].conteudo
         });
         
         var marker = new google.maps.Marker({
-          position: {lat:  bicho.latitude, lng: bicho.longitude},
+          position: {lat:  bicharedo[indicesorteado].latitude, lng: bicharedo[indicesorteado].longitude},
           map: map,
-          icon:bicho.icone,
+          icon:bicharedo[indicesorteado].icone,
           title: 'Hello World!'
         });
         
